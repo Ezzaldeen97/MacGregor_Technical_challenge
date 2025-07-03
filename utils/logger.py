72d5524ba2399022d5932d7f2972a360e180c4d2
Alribi <Alribi@faun.com>
2025-07-03 13:17:28 +0200

    BugFix: Logger will always save to the modubs logs files, this should be dynamic

--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,7 @@
 import logging
 
 
-def get_logger(name):
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s- %(levelname)s-%(message)s', filemode='a', filename='logs/modbus_client.log')
+def get_logger(name, file_name):
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s- %(levelname)s-%(message)s', filemode='a', filename=file_name)
     logger = logging.getLogger(name)
     return logger
