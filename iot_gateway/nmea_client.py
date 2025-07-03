d74f72dd1b93185077b29b76dd865ad83cb092df
Alribi <Alribi@faun.com>
2025-07-03 20:48:05 +0200

    1. Added a buffer to handle the case of multiple messages are sent on the same time, this will create a list of messages. 2. Checksum is now implemented to validate that the message that is recieved is correct. 3. Created a main entry point for the handler to interacte with the external Main.py script 4,=. Created a parse signal function that will be re-used often to evaluate messsage (DRY)

--- iot_gateway/nmea_client.py
+++ iot_gateway/nmea_client.py
@@ -1,6 +1,8 @@
 import socket
 import sys
 import os
+import operator
+from functools import reduce
 from datetime import datetime
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
@@ -17,14 +19,21 @@ class NmeaHandler:
     --------
     - port(int): Port Number of the NMEA server.
     - host (str): Host address of the NMEA server
+    - send_valid_data (bool): A flag to allow the handler to send only the data that is tagged with "Valid"
 
     """
     
-    def __init__(self, port:int, host:str) -> None:
+    def __init__(self, port:int, host:str,send_valid_data:bool=False) -> None:
         self.port = port
         self.host = host
         self.previous_datapoint=None
         self.connect()
+        self.buffer=""
+        self.send_valid_data=send_valid_data
+        if self.send_valid_data:
+            logger.info("Only Valid signals will be sent to the broker based on user wish")
+
+
 
     def connect(self) -> None:
         """
@@ -57,21 +66,22 @@ class NmeaHandler:
         try:
             data= self.sock.recv(1024).decode('ascii') #1 kilobyte
             if not data:
+                self.connect()
                 logger.error("Socket Closed by server")
             else:
-                return data
-            
+                messages= [f'${part}' for part in data.split('$') if part] # to recosntruct the messages.
+                return messages
         except Exception as e:
             logger.error(f"Unexpected error:{e}")
 
 
-    def parse_data(self, sentence:str)->dict:
+    def parse_data(self, messages:list)->dict:
         """
         Parses a given NMEA sentence
         
         Params:
         --------
-        - sentence(str): a NMEa statement that follows MG predefined format
+        - messages(list): a list of Nmea messages that follows MG predefined format
 
         Returns:
         --------
@@ -79,26 +89,70 @@ class NmeaHandler:
 
         
         """
+        def calculate_checksum(data: str) -> int:
+            """
+            A function to calculate the checksum of the message
 
-        try:
-            if not sentence.startswith('$') or '*' not in sentence:
-                logger.error(f"Invalid NMEA format {sentence}")
-                return
+            Args:
+            -----
+            - data(str): The raw data string without the "Given" checksum, eg. 
+            """
+            #https://gist.github.com/MattWoodhead/0bc2b3066796e19a3a350689b43b50ab
+            calculated_checksum = reduce(operator.xor, (ord(s) for s in data[1:]), 0) # [1:] to skip the $
+            return calculated_checksum
+
+        def parse_signal(message:str):
+            """
+            A function that encabsulates the handle of parsing a one signal
+
+            Args:
+            -----
+            -message(str): The one line of a simple Nmea Message. 
+
+            """
+            if not message.startswith('$') or '*' not in message:
+                    logger.error(f"Invalid NMEA format {message}")
+                    return
             else:
-                data, checksum = sentence.split("*")
-                fields= data.split(",")
-                talker_id = fields[0][1:3]
-                sentence_type= fields[0][3:]
-                rot_value= float(fields[1])
-                status= fields[2]
+                data, checksum = message.split("*")
+                calculated_checksum=calculate_checksum(data)
+                if int(checksum, base=16) != calculated_checksum:
+                    logger.error(f"Checksum mismatch. Received: {int(checksum, base=16)}, Calculated: {calculated_checksum}")
+                    return None
+                try:
+                    fields= data.split(",")
+                    talker_id = fields[0][1:3]
+                    sentence_type= fields[0][3:]
+                    rot_value= float(fields[1])
+                    status= fields[2]
+                    if status == "V" and self.send_valid_data:
+                        return
+                    datapoint = Datapoint_Nmea(talker_id, sentence_type, rot_value, status, checksum,self.previous_datapoint)
+                    self.previous_datapoint=datapoint
+                    return datapoint
+                except IndexError:
+                    logger.error("Error in index the nmea sentence")
+                except Exception as e:
+                    logger.error(f"Failed to parse nmea message {e}")     
 
-                datapoint = Datapoint_Nmea(talker_id, sentence_type, rot_value, status, checksum,self.previous_datapoint)
-                self.previous_datapoint=datapoint
-                return datapoint
-        except IndexError:
-            logger.error("Error in index the nmea sentence")
-        except Exception as e:
-            logger.error(f"Failed to parse nmea message {e}")       
+        datapoints=[]
+        for message in messages:
+            dp=parse_signal(message)
+            if dp:
+                datapoints.append(dp)
+        return datapoints
+  
+
+    def get_ROT_readings(self)->None:
+        """
+        A main entry point to get ROT readings from the websocket.
+
+        """
+        messages=self.read_nmea_data()
+        if messages:
+            return self.parse_data(messages)
+        
+
 
 
 class Datapoint_Nmea:
@@ -119,7 +173,6 @@ class Datapoint_Nmea:
         self.status=status
         self.checksum=checksum
         self.changed = True  
-
         self.send_point = True
         self.timestamp=datetime.now()
         self.evaluate_datapoint()
@@ -132,6 +185,7 @@ class Datapoint_Nmea:
         the data point will be marked as not for sending.
 
         """
+
         if not self.previous_datapoint: # The first data points (no previous) will be None
             return
         value_delta = abs(self.rot_value - self.previous_datapoint.rot_value)
@@ -139,8 +193,8 @@ class Datapoint_Nmea:
         if value_delta <= config.MIN_ROT_CHANGE and time_delta < config.MIN_ELAPSED_TIME:
             self.changed=False
             self.send_point=False 
-
-nmea_client=NmeaHandler(config.NMEA_PORT,config.NMEA_HOST)
-while True:
-    sentence=nmea_client.read_nmea_data()
-    nmea_client.parse_data(sentence)
+    def __str__(self):
+        date = self.timestamp.strftime("%Y-%m-%d")
+        time = self.timestamp.strftime("%H:%M")
+        status = "Valid" if self.status != "V" else "Invalid"
+        return f"{self.rot_value}, {status}, {date} at {time} UTC"
