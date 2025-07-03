ed3d184b99dc0911ef759f70fe98fe460cf9faaf
Alribi <Alribi@faun.com>
2025-07-03 10:34:36 +0200

    Added a config file that should contain all mb, mqtt, websocket parameters. Introduced a parser for the temperature sensors readings that will use an external mapper

---
+++ configs/mb_registers_mapping.py
@@ -0,0 +1,4 @@
+mapping ={0:"Luffing motor 1 temperature (PS Winch)",
+          1:"Luffing motor 2 temerature (STB Winch)",
+          2:"Luffing motor 3 temperature (PS Winch)",
+          3:"Luffing motor 4 temperature (STB Winch)"}
--- iot_gateway/modubs_tcp.py
+++ iot_gateway/modubs_tcp.py
@@ -1,19 +1,39 @@
 from pyModbusTCP.client import ModbusClient
 import time
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+from configs import config,mb_registers_mapping
 class MODBUS_CLIENT:
-    def __init__(self, ip:str, host: str, port:int, layer="TCP", unit_id=1):
+    def __init__(self, ip:str, host: str, port:int,unit_id:int, layer="TCP", ):
         self.ip=ip
         self.host=host
         self.port = port
         self.layer=layer
         self.unit_id = unit_id
-        self.connect()
+        self.connect() #will connect automatically.
     def connect(self):
         self.cnx=ModbusClient(host=self.host, port=self.port, unit_id=self.unit_id, auto_open=True)
-    def read_registers(self, register_address, number_of_registers):
-        return self.cnx.read_holding_registers(register_address, number_of_registers) #function code 3 (Read Multiple Holding Register) Hex0x03
+    def read_registers(self, register_address:int, number_of_registers:int):
+        self.values=self.cnx.read_holding_registers(register_address, number_of_registers) #function code 3 (Read Multiple Holding Register) 
+        print(f"Read values are:{self.values}")
+        return self.values
+    
+    def parse_readings(self):
 
-client = MODBUS_CLIENT(ip = "127.0.0.1" ,host="localhost", port=8889, unit_id=1)
+        for index, value in enumerate(self.values):
+            print(f"Sensor {mb_registers_mapping.mapping[index]} is {value} C")
+
+
+
+client = MODBUS_CLIENT(ip = config.MODBUS_IP ,host=config.MODBUS_HOST, port=config.MODUBS_PORT, unit_id=config.MODBUS_UNIT_ID)
+
+
+
+
 while True:
-    print(client.read_registers(0,4))
+    client.read_registers(0,4)
+    client.parse_readings()
     time.sleep(2)
