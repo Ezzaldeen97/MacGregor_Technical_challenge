13de7f17f414f48a8c0b27bc6ef95d8f4895f8dd
Alribi <Alribi@faun.com>
2025-07-03 11:09:31 +0200

    Added type hints, doc strings and removed redundant parameters

--- iot_gateway/modubs_tcp.py
+++ iot_gateway/modubs_tcp.py
@@ -6,16 +6,30 @@ from configs import config,mb_registers_mapping
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
-class MODBUS_CLIENT:
-    def __init__(self, ip:str, host: str, port:int,unit_id:int, layer="TCP", ):
-        self.ip=ip
+class ModbusClientHandler:
+    """
+    A Simple wrapper to handle Modbus TCP Communiction.
+
+    Params:
+    - Host(str): The Modbus Server host name.
+    - port(int): The Modbus port.
+    - unit_id(int): The device id.
+    - layer(str): The communication layer (default=TCP)
+    """
+    def __init__(self,  host: str, port:int,unit_id:int, layer:str="TCP" ):
         self.host=host
         self.port = port
-        self.layer=layer
+        self.layer=layer # Might be used in the future
         self.unit_id = unit_id
-        self.connect() #will connect automatically.
+        self.connect() #Automatic attempt to connect
 
-    def connect(self):
+    def connect(self) -> None:
+        """
+        A method to establish connection to the Modbus Server
+
+        Returns:
+            None
+        """
         try:
             self.cnx=ModbusClient(host=self.host, port=self.port, unit_id=self.unit_id, auto_open=True, timeout=0.1)
             if not self.cnx.is_open:
@@ -25,14 +39,30 @@ class MODBUS_CLIENT:
         except Exception as e:
             print(f"Error in connecting to Modbus server {e}")
 
-    def read_registers(self, register_address:int, number_of_registers:int):
-        self.values=self.cnx.read_holding_registers(register_address, number_of_registers) #function code 3 (Read Multiple Holding Register) 
+    def read_registers(self, register_address:int, number_of_registers:int) -> None:
+        """
+        A Method to read Modbus registers
+
+        Params:
+        - register_address(int): The Starting address of the register to read from.
+        - number_of_registers(int): The number of registers to read.
+        Returns:
+            None
+        """
+
+        self.values=self.cnx.read_holding_registers(register_address, number_of_registers) #function code 3 (Read Multiple Holding Register) Hex0x03
         if not self.values: # in case connection was lost during reading process
             self.connect()
             return
         print(f"Read values are:{self.values}")
     
-    def parse_readings(self):
+    def parse_readings(self) -> None:
+        """
+        A Method to parse and map the readings from the Modbus Server.
+
+        Returns:
+            None
+        """
         if not self.values:
             self.connect() # in case connection was lost during parsing process
             return
@@ -40,7 +70,7 @@ class MODBUS_CLIENT:
         for index, value in enumerate(self.values):
             print(f"Sensor {mb_registers_mapping.mapping[index]} is {value} C")
 
-client = MODBUS_CLIENT(ip = config.MODBUS_IP ,host=config.MODBUS_HOST, port=config.MODUBS_PORT, unit_id=config.MODBUS_UNIT_ID)
+client = ModbusClientHandler(host=config.MODBUS_HOST, port=config.MODUBS_PORT, unit_id=config.MODBUS_UNIT_ID)
 
 
 
