5771307c23f50a7346b6a222c8c2bbfa4594ed1d
Alribi <Alribi@faun.com>
2025-07-03 11:35:58 +0200

    Enhanced the logger format and solved a bug. Directed the out to a log file in case of errors instead of the console

--- utils/logger.py
+++ utils/logger.py
@@ -2,6 +2,6 @@ import logging
 
 
 def get_logger(name):
-    logging.basicConfig(level=logging.INFO, format='%(levelname)-%(message)')
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s- %(levelname)s-%(message)s', filemode='a', filename='logs/modbus_client.log')
     logger = logging.getLogger(name)
     return logger
