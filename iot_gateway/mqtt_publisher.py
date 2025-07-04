6ed338f52c8d33c4b8a5e067b5bdd51cce1436c4
Alribi <Alribi@faun.com>
2025-07-04 12:25:45 +0200

    Refactored a bit in the mqtt client and deleted un-needed code, added docstring

--- iot_gateway/mqtt_publisher.py
+++ iot_gateway/mqtt_publisher.py
@@ -16,15 +16,33 @@ iot_logger = get_logger("iot_gatway_logger", file_name='logs/iot_gatway.log')
 
 
 class MQTTPublisher:
+    """
+    MQTT Publisher class that manages MQTT client conenctions, 
+    publishes messages to different topics
+    """
+
+
     def __init__(self, host: str, port: int, username: str, password: str):
+        """
+        Initialize the MQTT Publisher client object.
+
+        Args:
+        -----
+        host(str): MQTT broker hostname
+        port(int): MQTT broker port number
+        username(str): Username for MQTT broker authentication
+        password(str): Password for MQTT broker authentication.
+
+
+
+        """
+        
         date_str = datetime.now().strftime("%d%m%y")
         client_id = f"ows-challenge-{date_str}"
         self.client = paho.Client(client_id=client_id, protocol=paho.MQTTv5)
-        self.client.username_pw_set(username, password)
+        self.client.username_pw_set(username, password) #NOTE: username/password are not required but essential for HiveMQ
         self.client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
         self.client.on_connect = self.on_connect
-        self.client.on_subscribe = self.on_subscribe
-        self.client.on_message = self.on_message
         self.client.on_publish = self.on_publish
         try:
             self.client.connect(host, port)
@@ -34,23 +52,40 @@ class MQTTPublisher:
             logger.error("Mqtt could not connect.")
             sys.exit(1)
     def on_connect(self,reason_code):
-        logger.info(f"[Connected] Reason code: {reason_code}")
+        logger.info(f"Connected Reason code: {reason_code}")
 
     def on_publish(self, mid, ):
-        logger.info(f"[Published] mid: {mid}")
+        logger.info(f"Published mid: {mid}")
 
-    def on_subscribe(self, mid, granted_qos,):
-        logger.info(f"[Subscribed] mid: {mid}, QoS: {granted_qos}")
-    def get_topic(self, message):
+    def get_topic(self, message:Datapoint|Datapoint_Nmea)->str:
+        """
+        Determine MQTT topic based on sensor datapoint object
+        
+        Args:
+        ----
+        message(Datapoint|Datapoint_Nmea): Message object to publish
+        
+        Rerunts:
+        --------
+        str: The topic string of the message
+        """
         if isinstance(message, Datapoint_Nmea) :
             return measurements_mapping.measurements_mapping.get("ROT")
         else:
             return measurements_mapping.measurements_mapping.get(message.sensor)
 
-    def on_message(self, msg):
-        logger.info(f"[Message] Topic: {msg.topic}, QoS: {msg.qos}, Payload: {msg.payload.decode()}")
-
     def publish(self, message: str, qos: int = 1):
+        """
+        Publish a message to the MQTT broker on appropriate topic
+
+        Args:
+        -----
+        message(str): The message to publish
+
+        qos(int, optional): Quality of service lever (deafult is 1 ) -> To fullfill the requirement that at least the message needs to be send once
+        """
+        
+        
         self.topic = self.get_topic(message)
         if message.send_point:
             logger.info(f"[Publishing] {message} to topic '{self.topic}'")
@@ -60,5 +95,9 @@ class MQTTPublisher:
             logger.info(f"Message {message} with topic {self.topic} cant be published as it didnt change or its invalid")
 
     def start(self):
+        """
+        Start the MQTT network loop.
+        
+        """
         logger.info("Starting MQTT loop")
         self.client.loop_forever()
