6307f2ddcf8f5614054cf5d3048cfcfd1fe8a2e9
Alribi <Alribi@faun.com>
2025-07-04 10:13:43 +0200

    Added a mapping for each sensor topic for better readability

---
+++ configs/measurements_mapping.py
@@ -0,0 +1,7 @@
+static_text="ows-challenge/mv-sinking-boat/main-crane"
+
+measurements_mapping={"Luffing motor 1 temperature (PS Winch)":f"{static_text}/luffing/temp-mot-1",
+          "Luffing motor 2 temerature (STB Winch)":f"{static_text}/luffing/temp-mot-2",
+          "Luffing motor 3 temperature (PS Winch)":f"{static_text}/luffing/temp-mot-3",
+          "Luffing motor 4 temperature (STB Winch)":f"{static_text}/luffing/temp-mot-4",
+          "ROT":f"{static_text}/rot"}
